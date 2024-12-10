ebb26217d1d25601537c12658d67acd67474a60c
Favour <gismozfexie@yahoo.com>
2024-12-10 17:41:12 +0100

    chore: minor mod

--- app/api/auth/passkey/register/route.ts
+++ app/api/auth/passkey/register/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import {
   generateRegistrationOptions,
   GenerateRegistrationOptionsOpts,
-  VerifiedRegistrationResponse,
   verifyRegistrationResponse,
   VerifyRegistrationResponseOpts,
 } from '@simplewebauthn/server';
